5d1fc6d9d03ed7bfc89eb48ba31be507e1168d8c
Boaz Ben-Dov <boaz.ben-dov@campus.technion.ac.il>
2018-05-03 14:04:18 +0200

    trying to implement database

--- app/src/main/java/com/example/alexbondar/cskarma2/OrganizationPage.java
+++ app/src/main/java/com/example/alexbondar/cskarma2/OrganizationPage.java
@@ -3,6 +3,7 @@ package com.example.alexbondar.cskarma2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 public class OrganizationPage extends AppCompatActivity {
@@ -10,6 +11,7 @@ public class OrganizationPage extends AppCompatActivity {
     TextView name;
     TextView description;
     TextView url;
+    ScrollView jobs;
 
 
     @Override
@@ -17,6 +19,7 @@ public class OrganizationPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organization_page);
 
+        /* some kind of black magic to get the organization details */
         name = findViewById(R.id.orgpage_name);
         name.setText(R.string.org_name);
     }
