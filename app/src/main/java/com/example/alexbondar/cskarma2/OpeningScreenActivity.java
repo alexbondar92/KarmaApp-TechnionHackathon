d19391040ce767e4fab3019bda499db948adcc8c
Ron <RonYitzhak25@gmail.com>
2018-05-03 18:31:12 +0300

    karmaBG2

--- app/src/main/java/com/example/alexbondar/cskarma2/OpeningScreenActivity.java
+++ app/src/main/java/com/example/alexbondar/cskarma2/OpeningScreenActivity.java
@@ -1,7 +1,11 @@
 package com.example.alexbondar.cskarma2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class OpeningScreenActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class OpeningScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opening_screen);
+    }
+
+    public void goToOrganization(View view) {
+        Intent intent = new Intent(getApplicationContext(),Register.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void goToUser(View view) {
+        //ImageButton answerTextView = (ImageButton)findViewById(R.id.organization);
+        Intent intent = new Intent(getApplicationContext(),UserLoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
