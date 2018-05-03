5a0c4fe7991d825b87b26b873fe0158ecc410333
Boaz Ben-Dov <boaz.ben-dov@campus.technion.ac.il>
2018-05-03 22:46:57 +0200

    organization page finished

--- app/src/main/java/com/example/alexbondar/cskarma2/OrganizationPage.java
+++ app/src/main/java/com/example/alexbondar/cskarma2/OrganizationPage.java
@@ -2,6 +2,7 @@ package com.example.alexbondar.cskarma2;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +10,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.net.URL;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,6 +22,7 @@ public class OrganizationPage extends AppCompatActivity {
     TextView name;
     TextView description;
     TextView url;
+    TextView nameAgain;
     ListView jobs;
 
     @Override
@@ -30,6 +33,7 @@ public class OrganizationPage extends AppCompatActivity {
         name = findViewById(R.id.orgpage_name);
         description = findViewById(R.id.orgpage_desc);
         url = findViewById(R.id.orgpage_website);
+        nameAgain = findViewById(R.id.orgpage_joblist_title_orgname);
         jobs = findViewById(R.id.orgpage_job_list);
 
         // some kind of black magic to get the organization.
@@ -44,6 +48,25 @@ public class OrganizationPage extends AppCompatActivity {
                 .collect(Collectors.toList())
                 .get(0);
         List<Job> these_jobs = this_org.getJobs();
+
+        name.setText(this_org.getName());
+        description.setText(this_org.getDesc());
+
+        String full_url = this_org.getWebsite();
+        if (!full_url.startsWith("http://") && !full_url.startsWith("https://"))
+            full_url = "http://" + full_url;
+
+        final String actual_url = full_url;
+        url.setText(full_url);
+        url.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent open_browser = new Intent(Intent.ACTION_VIEW, Uri.parse(actual_url));
+                startActivity(open_browser);
+                finish();
+            }
+        });
+        nameAgain.setText(this_org.getName());
 
         jobs.setAdapter(new CustomAdapter(this,DataParser.getAllJobs()));
         jobs.setOnItemClickListener(new AdapterView.OnItemClickListener() {
