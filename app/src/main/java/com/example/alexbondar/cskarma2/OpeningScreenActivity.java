d1cfb5776abb2b44fda9e265af91c6ad5a81db7a
Boaz Ben-Dov <boaz.ben-dov@campus.technion.ac.il>
2018-05-03 12:26:25 +0200

    empty activity for every screen

---
+++ app/src/main/java/com/example/alexbondar/cskarma2/OpeningScreenActivity.java
@@ -0,0 +1,13 @@
+package com.example.alexbondar.cskarma2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpeningScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opening_screen);
+    }
+}
