4d1cb5f859286e3205a38a30224870816324f964
alex bondar <alex.bondar@campus.technion.ac.il>
2018-05-03 20:03:44 +0300

    b4 meeting

--- app/src/main/java/com/example/alexbondar/cskarma2/JobActivity.java
+++ app/src/main/java/com/example/alexbondar/cskarma2/JobActivity.java
@@ -1,12 +1,20 @@
 package com.example.alexbondar.cskarma2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class JobActivity extends AppCompatActivity {
+    int job_id = getIntent().getExtras().getInt("id");
+
+    TextView myAwesomeTextView = (TextView)findViewById(R.id.currentJob);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        myAwesomeTextView.setText("My Awesome Text");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_job);
     }
