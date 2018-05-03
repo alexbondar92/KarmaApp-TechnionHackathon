a7f96bd571a1817e89acc26950da4afa5a7b8d2f
orgadhadas <orgad.hadas@gmail.com>
2018-05-03 21:29:24 +0300

    for boaz

--- app/src/main/java/com/example/alexbondar/cskarma2/DataParser.java
+++ app/src/main/java/com/example/alexbondar/cskarma2/DataParser.java
@@ -11,8 +11,11 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import static android.content.ContentValues.TAG;
 
@@ -39,11 +42,7 @@ public class DataParser {
                     Map<String, Object> o =(Map<String, Object>) db.get(i);
                     Map<String, Object> jobs = (Map<String, Object>) o.get("jobs");
 
-                    for (String j:jobs.keySet()) {
-
-                        Map<String, String> a_job=(Map<String, String>) jobs.get(j);
-                        list.add(new Job(j, Integer.parseInt(a_job.get("id")), a_job.get("desc"), a_job.get("pic"), a_job.get("region"), a_job.get("region"), a_job.get("data"), a_job.get("time_range"), a_job.get("type")));
-                        }
+                    list.addAll(parse_jobs(jobs));
                 }
             }
 
@@ -59,8 +58,53 @@ public class DataParser {
 
     }
 
+    public static Job getAJob(int id){
+        return getAllJobs().stream().filter(j->j.getId() == id).collect(Collectors.toList()).get(0);
+    }
+
     public static List<Organization> getAllOrganization(){
-        return null;
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+
+        final List<Organization> list = new ArrayList<>();
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Map<String, Object> db = (Map<String, Object>) dataSnapshot.getValue();
+                Map<String, Object> orgs = (Map<String, Object>) db.get("Organizations");
+
+                for (String i:orgs.keySet()
+                        ) {
+                    Map<String, Object> o =(Map<String, Object>) db.get(i);
+                    list.add(new Organization(i, (String) o.get("desc"), (String) o.get("website"), (String)o.get("type")));
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+
+
+        });
+        return list;
+    }
+
+    private static List<Job> parse_jobs(Map<String, Object> jobs){
+        List<Job> list = new ArrayList<>();
+        for (String j:jobs.keySet()) {
+
+            Map<String, String> a_job=(Map<String, String>) jobs.get(j);
+            list.add(new Job(j, Integer.parseInt(a_job.get("id")), a_job.get("desc"), a_job.get("pic"), a_job.get("region"), a_job.get("region"), a_job.get("data"), a_job.get("time_range"), a_job.get("type")));
+        }
+
+        return list;
     }
 
 }
