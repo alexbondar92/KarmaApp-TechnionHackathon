07c664d8b71115400730aeaa3561d8d1dbfe8b0d
nimkl <nimrod.k@campus.technion.ac.il>
2018-05-04 08:08:43 +0300

    reg ready

--- app/src/main/java/com/example/alexbondar/cskarma2/OrgDisplayPage.java
+++ app/src/main/java/com/example/alexbondar/cskarma2/OrgDisplayPage.java
@@ -2,14 +2,24 @@ package com.example.alexbondar.cskarma2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class OrgDisplayPage extends AppCompatActivity {
-
+    ListView listvv;
+    Button manage,addev;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_org_display_page);
 
-        List<>
+        listvv=(ListView)findViewById(R.id.listorgdis);
+        List<Job> southerns = DataParser.getAllJobs()
+                .stream().filter(j->j.getImg().equals("Latet")).collect(Collectors.toList());
+        final CustomAdapter customAdapter = new CustomAdapter(this, southerns);
+        listvv.setAdapter(customAdapter);
     }
 }
