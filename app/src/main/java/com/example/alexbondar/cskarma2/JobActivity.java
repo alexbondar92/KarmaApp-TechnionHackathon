d884f48d6def97d7d0e64d5917231a7076418086
orgadhadas <orgad.hadas@gmail.com>
2018-05-04 03:51:32 +0300

    kinda working

--- app/src/main/java/com/example/alexbondar/cskarma2/JobActivity.java
+++ app/src/main/java/com/example/alexbondar/cskarma2/JobActivity.java
@@ -1,7 +1,11 @@
 package com.example.alexbondar.cskarma2;
 
+import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class JobActivity extends AppCompatActivity {
@@ -32,5 +36,14 @@ public class JobActivity extends AppCompatActivity {
             time_and_place.setText(str);
             description.setText(current.getDesc());
         }
+
+        Button to_organization = (Button) findViewById(R.id.job_goto_org);
+        to_organization.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(),OrganizationPage.class);
+                startActivity(i);
+            }
+        });
     }
 }
